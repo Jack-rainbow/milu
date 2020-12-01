1cbbb90904ab9e146888d28ae3387e6d831697c4
jack-rainbow <yunchenlong@deepexi.com>
2020-12-01 14:53:00 +0800

    chore:  打包性能优化

--- config/configure-extend.js
+++ config/configure-extend.js
@@ -1,46 +1,86 @@
 /*eslint-disable */
-const { isProduction } = require("./utils")
-
+const { src } = require("./utils")
 module.exports = {
-  configureWebpack: {
-    // Webpack配置
-    devtool: "none", // webpack内关闭sourceMap
-    optimization: {
-      // 优化配置
-      splitChunks: {
-        chunks: "all",
-        cacheGroups: {
-          // 拆分Vue
-          vue: {
-            test: /[\\/]node_modules[\\/]vue[\\/]/,
-            name: "chunk-vue",
-          },
-        },
-      },
+    devtool: "none", // webpack内关闭sourceMap(不打包.js.map)文件
+    watch: true,
+    // 自定义监视模式
+    watchOptions: {
+        // 排除监听
+        ignored: /node_modules/,
+        // 监听到变化发生后，延迟 300ms（默认） 再去执行动作，
+        // 防止文件更新太快导致重新编译频率太高
+        aggregateTimeout: 300,
+        // 判断文件是否发生变化是通过不停的去询问系统指定文件有没有变化实现的
+        // 默认 1000ms 询问一次
+        poll: 1000,
     },
-  },
-  chainWebpack(config) {
-    if (isProduction) {
-      config.plugin("html").tap((args) => {
-        args[0].cdn = cdn
-        return args
-      })
-      config.externals(externals)
-      config.plugin("html").tap((args) => {
-        args[0].minify.minifyCSS = true
-        return args
-      })
-      // gzip需要nginx进行配合
-      config
-        .plugin("compression")
-        .use({})
-        .tap(() => [
-          {
-            test: /\.js$|\.html$|\.css/, // 匹配文件名
-            threshold: 10240, // 超过10k进行压缩
-            deleteOriginalAssets: true, // 是否删除源文件，这里最好不要删除
-          },
-        ])
-    }
-  },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: src,
+                use: ["thread-loader"],
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    "file-loader",
+                    {
+                        loader: "image-webpack-loader",
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65,
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.9],
+                                speed: 4,
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                                quality: 75,
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    optimization: {
+        // 优化配置
+        runtimeChunk: "single",
+        splitChunks: {
+            chunks: "all",
+            maxInitialRequests: Infinity,
+            minSize: 0,
+            cacheGroups: {
+                // 拆分Vue
+                vue: {
+                    test: /[\\/]node_modules[\\/]vue[\\/]/,
+                    name: "chunk-vue",
+                },
+                // 拆分Vendor
+                // vendor: {
+                //     test: /[\\/]node_modules[\\/]/,
+                //     name(module) {
+                //         // 获取第三方包名
+                //         const packageName = module.context.match(
+                //             /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+                //         )[1]
+
+                //         // npm 软件包名称是 URL 安全的，但是某些服务器不喜欢@符号
+                //         return `npm.${packageName.replace("@", "")}`
+                //     },
+                // },
+            },
+        },
+        minimizer: [],
+    },
 }
--- config/configure.webpack.js
+++ config/configure.webpack.js
@@ -1,41 +1,68 @@
 /*eslint-disable */
-const { isProduction } = require("./utils")
-const { name } = require("../package")
+const { isProduction, resolve } = require("./utils")
 const useAlias = require("./use-alias")
+const configureExtend = require("./configure-extend")
+// 打包包时间分析
+const webpack = require("webpack")
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin")
+const smp = new SpeedMeasurePlugin()
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin')
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
+const HardSourceWebpackPlugin = require("hard-source-webpack-plugin")
+const DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-plugin")
+
 
 // page title
 const PAGE_NAME = "米鹿"
 
-// externals lib key
+// 打包时排除  模块
 const externals = {
-  vue: "Vue",
-  vuex: "Vuex",
-  axios: "axios",
+    vue: "Vue",
+    vuex: "Vuex",
+    axios: "axios",
 }
 
-const config = {
-  name: PAGE_NAME,
-  output: {
-    // 把子应用打包成 umd 库格式
-    library: `${name}-[name]`,
-    libraryTarget: "umd",
-    jsonpFunction: `webpackJsonp_${name}`,
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".vue", ".json", "ts", "tsx"],
-    alias: {
-      set(name, path) {
-        this[name] = path
-        return this
-      },
+const config = smp.wrap({
+    name: PAGE_NAME,
+    output: {
+        filename: "bundle.js",
     },
-  },
-  externals: isProduction ? externals : {},
-  // 在生产环境下为 Babel 和 TypeScript 使用 `thread-loader`
-  // 在多核机器下会默认开启。
-  // parallel: require("os").cpus().length > 1,
-  plugins: [],
-}
+    resolve: {
+        extensions: [".js", ".jsx", ".vue", ".json", "ts", "tsx"],
+        alias: {
+            set(name, path) {
+                this[name] = path
+                return this
+            },
+        },
+    },
+    externals: isProduction ? externals : {},
+    // 在生产环境下为 Babel 和 TypeScript 使用 `thread-loader`
+    // 在多核机器下会默认开启。
+    // parallel: require("os").cpus().length > 1,
+    plugins: [
+        // 提升二次构建速度
+        new HardSourceWebpackPlugin(),
+
+        new BundleAnalyzerPlugin(),
+        // 查找重复包
+        new DuplicatePackageCheckerPlugin(),
+
+        //使用 DLLPlugin 进行分包
+        new webpack.DllPlugin({
+            name: "_dll_[name]", //该字段的值也就是输出的 manifest.json 文件 中 name 字段的值 例如 library.manifest.json 中就有 "name": "_dll_library"
+            path: resolve("./build/library/[name].manifest.json"), //
+        }),
+
+        // moment  只打包中文包
+        new MomentLocalesPlugin({
+            localesToKeep: ["zh-cn"],
+        }),
+
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/), // 配置忽略规则
+    ],
+    ...configureExtend,
+})
 
 useAlias(config)
 delete config.resolve.alias.set
